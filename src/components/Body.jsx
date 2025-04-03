e295067903d8c5e361946ecd9985eb418191d107
kunal <kunalwaldia8@gmail.com>
2025-04-03 19:01:45 +0530

    add environment variable support; update Firebase configuration to use environment variables; replace old CSS files with new versions

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,21 +4,27 @@ import { auth } from "../utils/firebase";
 
 export const Body = () => {
   const handleSignOut = () => {
-    signOut(auth).then(() => {
-      console.log("Signed out successfully");
-    }).catch((error) => {
-      console.log(error);
-    });
-  }
+    signOut(auth)
+      .then(() => {
+        console.log("Signed out successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div>
       <div className="absolute inset-0 z-0">
-        {/* <img
-          src="https://assets.nflxext.com/ffe/siteui/vlv3/fbf440b2-24a0-49f5-b2ba-a5cbe8ea8736/web/IN-en-20250324-TRIFECTA-perspective_d7c906ec-0531-47de-8ece-470d5061c88a_medium.jpg"
-          alt="bg-img"
+        <img
+          src={import.meta.env.VITE_IMG_CDN_URL}
           className="w-full h-full object-cover"
-        /> */}
-        <button className="absolute top-50 right-0 text-xl font-bold pointer-cursor text-black" onClick={handleSignOut}>Sign Out</button>
+        />
+        <button
+          className="absolute top-50 right-0 text-xl font-bold pointer-cursor text-black"
+          onClick={handleSignOut}
+        >
+          Sign Out
+        </button>
       </div>
     </div>
   );
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA2KaT0vthoN26YKGuwfNT6icWIyLky6xw",
-  authDomain: "netflix-gpt-2496e.firebaseapp.com",
-  projectId: "netflix-gpt-2496e",
-  storageBucket: "netflix-gpt-2496e.firebasestorage.app",
-  messagingSenderId: "927821295497",
-  appId: "1:927821295497:web:179c9777544d5611047b58",
-  measurementId: "G-EW3NBQ0B10",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
